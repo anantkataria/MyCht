fa776cec9af2fcaf5c99b258f4971428f237df7b
Anant kataria <51715043+savage-9@users.noreply.github.com>
2019-12-11 09:43:34 +0530

    added functionality of retrieving messages back from firebase to the UI of the user

--- app/src/main/java/com/anantdevelopers/mychat/MainActivity.java
+++ app/src/main/java/com/anantdevelopers/mychat/MainActivity.java
@@ -1,10 +1,16 @@
 package com.anantdevelopers.mychat;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.method.TextKeyListener;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +18,9 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -37,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
      private ProgressBar mProgressBar;
 
      private String mUsername;
+
+     private ChildEventListener mChildEventListener;
 
      @Override
      protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +77,39 @@ public class MainActivity extends AppCompatActivity {
                     myClass myClass = new myClass(mMessageEditText.getText().toString(), "anonymous", null);
                     String unique_id = databaseReference.push().getKey();
                     databaseReference.child(unique_id).setValue(myClass);
+
+                    TextKeyListener.clear(mMessageEditText.getText());
                }
           });
+
+          //after data at given reference is added, the childeventlistener's onchildadded method will get invoked in which
+          //we provide the code to show messages back to the UI using adapter/listview
+          mChildEventListener = new ChildEventListener() {
+               @Override
+               public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    myClass myClass = dataSnapshot.getValue(myClass.class);
+                    mMessageAdapter.add(myClass);
+               }
+
+               @Override
+               public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+               }
+
+               @Override
+               public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+               }
+
+               @Override
+               public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+               }
+
+               @Override
+               public void onCancelled(@NonNull DatabaseError databaseError) {
+               }
+          };
+
+          databaseReference.addChildEventListener(mChildEventListener);
+
 
           mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
                @Override
@@ -95,9 +137,17 @@ public class MainActivity extends AppCompatActivity {
                }
           });
 
+     }
 
+     @Override
+     public boolean onCreateOptionsMenu(Menu menu) {
+          MenuInflater inflater = getMenuInflater();
+          inflater.inflate(R.menu.main_menu, menu);
+          return true;
+     }
 
-
-
+     @Override
+     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+          return super.onOptionsItemSelected(item);
      }
 }
